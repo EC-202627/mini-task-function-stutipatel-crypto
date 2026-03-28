1733eb995df7c12176d5af5130cca26fe8320eb4
stutipatel-crypto <stuti.patel@avantika.edu.in>
2026-03-28 08:50:16 +0000

    pb5

--- Problem_05.py
+++ Problem_05.py
@@ -1,1 +1,23 @@
 
+def calculate_fine(book_title, days_overdue, daily_rate=5.0, max_fine=150.0):
+    fine = days_overdue * daily_rate
+
+    if fine > max_fine:
+        fine = max_fine
+
+    return fine
+
+
+# Input (line by line)
+book_title = input()
+days = int(input())
+rate = float(input())
+max_fine = float(input())
+
+# Function call
+fine = calculate_fine(book_title, days, rate, max_fine)
+
+# Output
+print(f"Book: {book_title}")
+print(f"Days overdue: {days}")
+print(f"Fine: Rs. {fine}")
